11754031106dcd4ceb15e02a4983686128b60047
Justin Baum <justinbaum5@gmail.com>
2022-11-29 20:34:05 -0500

    Prisoners Game

---
+++ _prisoners/precache-manifest.f165d10c55b025ab3fba1dea2dc81a9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9977e0e3d48aeab8c91b",
+    "url": "/static/css/main.8d724459.chunk.css"
+  },
+  {
+    "revision": "9977e0e3d48aeab8c91b",
+    "url": "/static/js/main.9977e0e3.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "/static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "974a775549b5cfba08ed",
+    "url": "/static/js/2.974a7755.chunk.js"
+  },
+  {
+    "revision": "f42220c51556092d1319",
+    "url": "/static/js/runtime~main.f42220c5.js"
+  },
+  {
+    "revision": "25793754a55fcd3a6a0ea6168da18868",
+    "url": "/index.html"
+  }
+];
---
+++ _prisoners/precache-manifest.f8a1e1d573fafa027434d057dc3f6f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3894c3762bfd0952fe",
+    "url": "/static/css/main.8d724459.chunk.css"
+  },
+  {
+    "revision": "bf3894c3762bfd0952fe",
+    "url": "/static/js/main.bf3894c3.chunk.js"
+  },
+  {
+    "revision": "7483025c10a0f8f622e0",
+    "url": "/static/js/1.7483025c.chunk.js"
+  },
+  {
+    "revision": "974a775549b5cfba08ed",
+    "url": "/static/js/2.974a7755.chunk.js"
+  },
+  {
+    "revision": "f42220c51556092d1319",
+    "url": "/static/js/runtime~main.f42220c5.js"
+  },
+  {
+    "revision": "3a7950114bd32dc372d9d3618c15bc2c",
+    "url": "/index.html"
+  }
+];
